a0c7a2c396a9d61fc9d2f3515e14d85700f02a60
abirsalloum <143286267+abirsalloum@users.noreply.github.com>
2026-01-19 12:23:31 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,11 @@
-return {
+import { defineConfig, loadEnv } from 'vite'
+import react from '@vitejs/plugin-react'
+import process from 'node:process'
+
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+
+  return {
   base: '/Smart-College/',
   plugins: [react()],
   build: {
@@ -11,3 +18,5 @@ return {
     port: 3000
   }
 }
+
+})
